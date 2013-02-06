f1c81fad809a0ef511c37f86b30e4505e9690783
miningold <rynldsrap@gmail.com>
2013-02-05 16:48:17 -0800

    add create room

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+app.post('/level', data.createRoom);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
--- routes/data.js
+++ routes/data.js
@@ -11,10 +11,35 @@ MongoClient.connect(mongoURL, function(err, db) {
     return;
   }
 
-  collection = db.collection('player');
+  collection = db.collection('level');
 
   // Empty the collection for debugging
   collection.remove(function(err) {
     if (err) throw err;
   });
 });
+
+exports.createRoom = function(req, res, next) {
+  MongoClient.connect(mongoURL, function(err, db) {
+    var doc;
+
+    if (err) {
+      console.dir(err);
+      return next(err);
+    }
+
+    doc = req.body;
+
+    collection = db.collection('level');
+
+    // console.dir(doc);
+
+    collection.insert(doc, {w:1}, function(err, result) {
+      if (err) {
+        console.dir(err);
+      }
+
+      res.send(201, result[0]);
+    });
+  });
+};
