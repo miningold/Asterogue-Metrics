d339585fc8270dc161dc6380162c37ce9919f2ca
miningold <rynldsrap@gmail.com>
2013-07-02 10:24:33 -0700

    add avarage time played histogram

--- public/js/main.js
+++ public/js/main.js
@@ -32,6 +32,8 @@ var drawSession = function(data) {
   data = _.groupBy(data, 'uniqueId');
 
   drawPlayTime(data);
+
+  drawTimeHistogram(data);
 };
 
 var drawPlayTime = function(data) {
@@ -84,7 +86,7 @@ var drawPlayTime = function(data) {
 
   chart.domain([min, max]);
 
-  var svg = d3.select('body').selectAll('svg')
+  var svg = d3.select('#box').selectAll('svg')
       .data(data)
     .enter().append('svg')
       .attr('class', 'box')
@@ -106,6 +108,92 @@ var iqr = function(k) {
     while (d[--j] > q3 + iqr);
     return [i, j];
   };
+};
+
+var drawTimeHistogram = function(data) {
+  var max = 0;
+
+  // map from object to array
+  data = _.map(data, function(value, key) {
+    return value;
+  });
+
+  _.each(data, function(sessions, index) {
+
+    // filter out raid and tutorial
+    sessions = _.filter(sessions, function(session) {
+      return !(session.raidMode || session.tutorial);
+    });
+
+    // calculate average
+    data[index] = _.reduce(sessions, function(memo, session) {
+      return memo + session.timePlayed;
+    }, 0) / sessions.length;
+
+    // record max
+    if (data[index] > max) {
+      max = data[index];
+    }
+  });
+
+  // find nearest multiple of 60 greater than max
+  max = Math.ceil(max / 60) * 60;
+
+  // Formatters for counts and times (converting numbers to Dates).
+  var formatCount = d3.format(",.0f"),
+      formatTime = d3.time.format("%H:%M"),
+      formatMinutes = function(d) { return formatTime(new Date(2012, 0, 1, 0, d)); };
+
+  var margin = {top: 10, right: 30, bottom: 30, left: 30},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+
+  var x = d3.scale.linear()
+      .domain([0, max])
+      .range([0, width]);
+
+  // Generate a histogram using twenty uniformly-spaced bins.
+  var data = d3.layout.histogram()
+      .bins(x.ticks(20))
+      (data);
+
+  var y = d3.scale.linear()
+      .domain([0, d3.max(data, function(d) { return d.y; })])
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickFormat(formatMinutes);
+
+  var svg = d3.select("#bars").append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+      .data(data)
+    .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+  bar.append("rect")
+      .attr("x", 1)
+      .attr("width", x(data[0].dx) - 1)
+      .attr("height", function(d) { return height - y(d.y); });
+
+  bar.append("text")
+      .attr("dy", ".75em")
+      .attr("y", 6)
+      .attr("x", x(data[0].dx) / 2)
+      .attr("text-anchor", "middle")
+      .text(function(d) { return formatCount(d.y); });
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
 };
 
 
